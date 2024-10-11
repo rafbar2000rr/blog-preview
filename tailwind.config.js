274dbdef78fbea56c12e160377bdd57171e48c11
Rafael Ibarra <rafbar2000rr@gmail.com>
2024-10-11 00:40:39 -0500

    primer envío

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {colors: {
+      
+      Yellow: 'hsl(47, 88%, 63%)',
+      White: 'hsl(0, 0%, 100%)',
+      
+    },
+          fontFamily: {
+      figtree: ['Figtree', 'sans-serif'],
+    }
+    }
+  },
+  plugins: [],
+  
+
+}
